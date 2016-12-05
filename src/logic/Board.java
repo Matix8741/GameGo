7085578fca66f359da32291430a6330eb0aa68ea
Darlenit <darlenit@gmail.com>
2016-12-05 18:15:56 +0100

    tyle pracy wow

--- src/logic/Board.java
+++ src/logic/Board.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 public class Board {
 	
+	private int size;
 	private Field[][] fields;
+
 	
-	public Board() {
-		fields = new Field[19][19];
+	public Board(int size) {
+		this.size = size;
+		fields = new Field[size][size];
+		for (int i=0; i<size; i++)
+			for (int j=0; j<size; j++)
+				field[i][j] = new Field(i+1, j+1);
 	}
 }
